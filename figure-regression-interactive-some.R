f48ac4870343e496f148c2ab1565e4f461fb772c
Toby Dylan Hocking <tdhock5@gmail.com>
2017-02-09 17:12:04 -0500

    facet regression plots

--- figure-regression-interactive-some.R
+++ figure-regression-interactive-some.R
@@ -43,6 +43,10 @@ feature.name.vec <- c(
 train.dt.list <- list()
 totals.list <- list()
 reg.line.list <- list()
+roc.list <- list()
+auc.list <- list()
+pred.dot.list <- list()
+auc.polygon.list <- list()
 for(model.name in names(feature.name.vec)){
   model.label <- model.label.vec[[model.name]]
   feature.name <- feature.name.vec[[model.name]]
@@ -70,10 +74,31 @@ for(model.name in names(feature.name.vec)){
     intervals=.N
     ), by=.(model.name, model.label, sign.residual=sign(residual))]
   train.dt.list[[model.name]] <- model.train.dt
+  feature.result <- ROChange(
+    errors$model.errors, model.train.dt, c("profile.id", "chromosome"))
+  pred.dot.list[[model.name]] <- data.table(
+    model.name, model.label, feature.result$thresholds)
+  feature.result$roc[, threshold := {
+    ifelse(
+      min.thresh < 0 & 0 < max.thresh,
+      "predicted", ifelse(
+        errors==min(errors),
+        "min.error", "other"))
+  }]
+  roc.list[[model.name]] <- data.table(
+    model.name, model.label, feature.result$roc)
+  auc.list[[model.name]] <- data.table(
+    model.name, model.label, auc=feature.result$auc)
+  auc.polygon.list[[model.name]] <- data.table(
+    model.name, model.label, feature.result$auc.polygon)
 }
 train.dt <- do.call(rbind, train.dt.list)
 totals <- do.call(rbind, totals.list)
 reg.line <- do.call(rbind, reg.line.list)
+roc <- do.call(rbind, roc.list)
+auc <- do.call(rbind, auc.list)
+pred.dot <- do.call(rbind, pred.dot.list)
+auc.polygon <- do.call(rbind, auc.polygon.list)
 gg <- ggplot()+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "lines"))+
@@ -104,106 +129,77 @@ gg <- ggplot()+
   scale_y_continuous("<-- more changes     log(penalty)     less changes -->")
 print(gg)
 
-
-
-model.list <- list(
-  BIC=train.dt,
-  learned=pred.dt)
-roc.list <- list()
-auc.list <- list()
-pred.dot.list <- list()
-auc.polygon.list <- list()
-for(model.name in names(model.list)){
-  model.dt <- model.list[[model.name]]
-  feature.result <- ROChange(errors$model.errors, model.dt, c("profile.id", "chromosome"))
-  pred.dot.list[[model.name]] <- data.table(model.name, feature.result$thresholds)
-  roc.list[[model.name]] <- data.table(model.name, feature.result$roc)
-  auc.list[[model.name]] <- data.table(model.name, auc=feature.result$auc)
-  auc.polygon.list[[model.name]] <- data.table(model.name, feature.result$auc.polygon)
-}
-roc <- do.call(rbind, roc.list)
-auc <- do.call(rbind, auc.list)
-pred.dot <- do.call(rbind, pred.dot.list)
-auc.polygon <- do.call(rbind, auc.polygon.list)
-
-roc[, mid.thresh := (min.thresh+max.thresh)/2]
+w <- roc[is.finite(min.thresh), abs(mean(diff(min.thresh)))]
+roc[, prev.thresh := ifelse(min.thresh==-Inf, max.thresh-w, min.thresh)]
+roc[, next.thresh := ifelse(max.thresh==Inf, min.thresh+w, max.thresh)]
+roc[, mid.thresh := (prev.thresh+next.thresh)/2]
 pred.dot[, mid.thresh := (min.thresh+max.thresh)/2]
-roc[, threshold := "other"]
-## We would like to sample some thresholds evenly in the ROC space, so
-## here we compute dist.from.zero, which is the cumulative euclidean
-## distance from (0,0) in the ROC space.
-roc[, dist.from.zero := cumsum(sqrt({
-  diff(c(0, FPR))^2 + diff(c(0, TPR))^2
-})), by=model.name]
-grid.thresh <- roc[is.finite(mid.thresh), {
-  .SD[as.integer(seq(1, .N, l=50)[c(1,5,10,15,20, 25, 30:50)]), ]
-}, by=model.name]
-grid.thresh <- roc[is.finite(mid.thresh), {
-  r <- range(dist.from.zero)
-  d.vec <- seq(r[1], r[2], l=50)
-  i.vec <- sapply(d.vec, function(d)which.min(abs(dist.from.zero-d)))
-  .SD[i.vec,]
-}, by=model.name]
-some.thresh <- unique(rbind(
-  grid.thresh[, names(pred.dot), with=FALSE],
-  pred.dot))
-setkey(some.thresh, model.name, mid.thresh)
-some.thresh[, prev.thresh := {
-  d <- diff(mid.thresh)/2
-  c(mid.thresh[1]-d[1], mid.thresh[-.N]+d)
-  }, by=model.name]
-some.thresh[, next.thresh := {
-  d <- diff(mid.thresh)/2
-  c(mid.thresh[-.N]+d, mid.thresh[.N]+d[.N-1])
-}, by=model.name]
-some.thresh[, stopifnot(
-  identical(prev.thresh[-1], next.thresh[-.N])
-  ), by=model.name]
-some.thresh[, slope := ifelse(model.name=="BIC", 1, learned.slope)]
-some.thresh[, intercept := {
-  ifelse(model.name=="BIC", 0, learned.intercept)+mid.thresh
+setkey(train.dt, model.name, model.label)
+setkey(roc, model.name, model.label)
+some.thresh.pred <- train.dt[roc, allow.cartesian=TRUE]
+some.thresh.pred[, pred.plus.thresh := pred.log.lambda + mid.thresh]
+some.thresh.pred[, residual := {
+  targetIntervalResidual(cbind(min.L, max.L), pred.plus.thresh)
 }]
-
-both.pred <- rbind(train.dt, pred.dt)
-setkey(both.pred, model.name)
-setkey(some.thresh, model.name)
-some.thresh.pred <- both.pred[some.thresh, allow.cartesian=TRUE]
-some.thresh.pred[, pred.log.lambda := slope * feature + intercept]
-some.thresh.pred[, residual := targetIntervalResidual(cbind(min.L, max.L), pred.log.lambda)]
 total.thresh.pred <- some.thresh.pred[, list(
   total.residual=sum(residual),
   intervals=.N
-  ), by=.(model.name, sign.residual=sign(residual), mid.thresh)]
+  ), by=.(model.name, model.label, sign.residual=sign(residual), mid.thresh)]
 total.thresh.pred
 residual.thresh.pred <- some.thresh.pred[residual!=0,]
+space.dt <- rbind(
+  BIC=data.table(model.name="BIC", left=1.55, top=4.5, space=0.5),
+  learned=data.table(model.name="learned", left=-3.3, top=4.5, space=0.5))
+setkey(space.dt, model.name)
+setkey(total.thresh.pred, model.name)
+total.labels <- total.thresh.pred[space.dt]
+total.labels[, log.penalty := top-(1-sign.residual)*space]
+all.reg.lines <- reg.line[roc, on=.(model.name, model.label)]
 
-auc$TPR <- c(0.85, 0.9)
-auc$FPR <- 0.2
-bic.left <- 1.45
-bic.top <- 6.5
-bic.space <- 0.7
-learned.left <- 1.8
-learned.top <- -4
-thresh.colors <- c(predicted="green", min.error="black", other="white")
+model.colors <- c(
+  learned="blue",
+  BIC="red"
+  )
+ggplot()+
+  theme_bw()+
+  theme(panel.margin=grid::unit(0, "lines"))+
+  ##facet_grid(. ~ panel.label, scales="free")+
+  scale_color_manual(values=model.colors)+
+  geom_segment(aes(prev.thresh, errors,
+                   color=model.name,
+                   xend=next.thresh, yend=errors),
+               data=roc)
+
+auc[, TPR := c(0.85, 0.9)]
+auc[, FPR := 0.2]
+thresh.colors <- c(predicted="green", "min.error"="black", other="white")
 viz <- list(
   title="BIC versus learned penalty in neuroblastoma data",
   thresholds=ggplot()+
     theme_bw()+
     guides(fill="none", color="none")+
-    geom_line(aes(mid.thresh, errors, group=model.name, color=model.name,
-                  showSelected=model.name), data=some.thresh)+
     geom_tallrect(aes(xmin=prev.thresh, xmax=next.thresh,
                       clickSelects.variable=paste0(model.name, ".thresh"),
                       clickSelects.value=mid.thresh,
-                      color=model.name,
                       showSelected=model.name),
-                  size=3,
-                  alpha=0.5,
-                  data=some.thresh)+
+                  alpha=0.2,
+                  data=roc)+
+    geom_vline(aes(xintercept=mid.thresh, color=model.name,
+                   showSelected.variable=paste0(model.name, ".thresh"),
+                   showSelected.value=mid.thresh,
+                   showSelected=model.name),
+               data=roc)+
+    geom_segment(aes(prev.thresh, errors,
+                     color=model.name,
+                     clickSelects=model.name,
+                     xend=next.thresh, yend=errors),
+                 alpha=0.8,
+                 size=4,
+                 data=roc)+
     ylab("total incorrect labels")+
     xlab("constant/threshold added to log(penalty)")+
     scale_fill_manual(values=thresh.colors)+
-    scale_color_manual(values=c(BIC="red", learned="deepskyblue"))+
+    scale_color_manual(values=model.colors)+
     geom_point(aes((max.thresh+min.thresh)/2, errors,
                    clickSelects.variable=paste0(model.name, ".thresh"),
                    clickSelects.value=mid.thresh,
@@ -211,114 +207,95 @@ viz <- list(
                    showSelected=model.name,
                    showSelected2=threshold,
                    fill=threshold),
-               data=pred.dot,
+               data=roc[threshold!="other",],
                size=4,
                shape=21),
   roc=ggplot()+
     theme_bw()+
-    geom_text(aes(FPR, TPR, color=model.name, label=sprintf("AUC = %.4f", auc)),
+    ##guides(color="none")+
+    geom_text(aes(FPR, TPR,
+                  clickSelects=model.name,
+                  color=model.name, label=sprintf("AUC = %.4f", auc)),
               hjust=0,
               data=auc)+
-    geom_path(aes(FPR, TPR, color=model.name, group=model.name),
-              size=2,
+    geom_path(aes(FPR, TPR, clickSelects=model.name,
+                  color=model.name, group=model.name),
+              size=4,
+              alpha=0.8,
               data=roc)+
     scale_fill_manual(values=thresh.colors, breaks=names(thresh.colors))+
-    scale_color_manual(values=c(BIC="red", learned="deepskyblue"))+
+    scale_color_manual(values=model.colors, breaks=names(model.colors))+
+    ## geom_point(aes(FPR, TPR,
+    ##                showSelected.variable=paste0(model.name, ".thresh"),
+    ##                showSelected.value=mid.thresh,
+    ##                showSelected2=threshold,
+    ##                showSelected=model.name),
+    ##            data=roc,
+    ##            color="grey",
+    ##            size=6,
+    ##            shape=21)+
     geom_point(aes(FPR, TPR,
-                   showSelected.variable=paste0(model.name, ".thresh"),
-                   showSelected.value=mid.thresh,
-                   showSelected2=threshold,
-                   showSelected=model.name),
-               data=some.thresh,
-               color="grey",
-               size=6,
-               shape=21)+
-    geom_point(aes(FPR, TPR,
+                   showSelected=model.name,
                    clickSelects.variable=paste0(model.name, ".thresh"),
                    clickSelects.value=mid.thresh,
                    color=model.name, fill=threshold),
-               data=some.thresh,
+               data=roc,
                size=4,
-               alpha=0.8,
+               alpha=0.7,
                shape=21)+
     coord_equal(),
   regression=ggplot()+
     theme_bw()+
-    theme_animint(width=800)+
+    theme(panel.margin=grid::unit(0, "lines"))+
+    facet_grid(. ~ model.label, scales="free")+
+    theme_animint(width=600)+
     geom_text(aes(
-      bic.left, bic.top, color=model.name,
+      left, log.penalty, 
+      color=model.name,
       showSelected.variable=paste0(model.name, ".thresh"),
       showSelected.value=mid.thresh,
-      label=sprintf(
-        "total too high = %.1f (%d intervals / %d possible)",
-        total.residual, intervals, possible$negative)),
-              data=total.thresh.pred[sign.residual==1 & model.name=="BIC", ],
+      label=ifelse(sign.residual==0, sprintf(
+        "%d intervals correctly predicted", intervals), sprintf(
+        "total too %s = %.1f (%d/%d intervals)",
+          ifelse(sign.residual==1, "high", "low"),
+          total.residual, intervals,
+          possible[, ifelse(sign.residual==1, negative, positive)]))),
+              data=total.labels,
               hjust=0)+
-    geom_text(aes(
-      bic.left, bic.top-bic.space, color=model.name,
-      showSelected.variable=paste0(model.name, ".thresh"),
-      showSelected.value=mid.thresh,
-      label=sprintf("%d intervals correctly predicted", intervals)),
-              data=total.thresh.pred[sign.residual==0 & model.name=="BIC", ],
-              hjust=0)+
-    geom_text(aes(
-      bic.left, bic.top-bic.space*2, color=model.name,
-      showSelected.variable=paste0(model.name, ".thresh"),
-      showSelected.value=mid.thresh,
-      label=sprintf(
-        "total too low = %.1f (%d intervals / %d possible)",
-        total.residual, intervals, possible$positive)),
-              data=total.thresh.pred[sign.residual==-1 & model.name=="BIC", ],
-              hjust=0)+
-    geom_text(aes(
-      learned.left, learned.top, color=model.name,
-      showSelected.variable=paste0(model.name, ".thresh"),
-      showSelected.value=mid.thresh,
-      label=sprintf(
-        "total too high = %.1f (%d intervals / %d possible)",
-        total.residual, intervals, possible$negative)),
-              data=total.thresh.pred[sign.residual==1 & model.name=="learned", ],
-              hjust=0)+
-    geom_text(aes(
-      learned.left, learned.top-bic.space, color=model.name,
-      showSelected.variable=paste0(model.name, ".thresh"),
-      showSelected.value=mid.thresh,
-      label=sprintf("%d intervals correctly predicted", intervals)),
-              data=total.thresh.pred[sign.residual==0 & model.name=="learned", ],
-              hjust=0)+
-    geom_text(aes(
-      learned.left, learned.top-bic.space*2, color=model.name,
-      showSelected.variable=paste0(model.name, ".thresh"),
-      showSelected.value=mid.thresh,
-      label=sprintf(
-        "total too low = %.1f (%d intervals / %d possible)",
-        total.residual, intervals, possible$positive)),
-              data=total.thresh.pred[sign.residual==-1 & model.name=="learned", ],
-              hjust=0)+
-    geom_point(aes(feature, min.L, fill=limit),
+    geom_point(aes(feature, min.L,
+                   ##showSelected=model.name,
+                   fill=limit),
                data=data.table(limit="min", train.dt[is.finite(min.L),]),
                shape=21)+
-    geom_point(aes(feature, max.L, fill=limit),
+    geom_point(aes(feature, max.L,
+                   ##showSelected=model.name,
+                   fill=limit),
                data=data.table(limit="max", train.dt[is.finite(max.L),]),
                shape=21)+
     geom_abline(aes(slope=slope,
+                    ##showSelected=model.name,
+                    color=model.name,
                     showSelected.variable=paste0(model.name, ".thresh"),
                     showSelected.value=mid.thresh,
-                    intercept=intercept, color=model.name),
-                data=some.thresh)+
+                    intercept=intercept+mid.thresh),
+                data=all.reg.lines)+
+    guides(color="none")+
     geom_segment(aes(
-      feature, pred.log.lambda, xend=feature, color=model.name,
+      feature, pred.plus.thresh,
+      xend=feature, yend=pred.plus.thresh-residual, 
+      ##showSelected=model.name,
+      color=model.name,
       showSelected.variable=paste0(model.name, ".thresh"),
-      showSelected.value=mid.thresh,
-      yend=pred.log.lambda-residual),
+      showSelected.value=mid.thresh),
                  size=1,
                  data=residual.thresh.pred)+
     scale_fill_manual(values=c(min="black", max="white"))+
-    scale_color_manual(values=c(BIC="red", learned="deepskyblue"))+
-    scale_x_continuous("feature = log(log(n = number of data points to segment))")+
-    scale_y_continuous("<-- more changes     log(penalty)     less changes -->"),
-  first=list())
-pred.thresh.only <- some.thresh[threshold=="predicted",]
+    scale_color_manual(values=model.colors)+
+    scale_x_continuous("feature")+
+    scale_y_continuous("<-- more changes log(penalty) less changes -->"),
+  first=list(),
+  selector.types=list(model.name="single"))
+pred.thresh.only <- roc[min.thresh < 0 & 0 < max.thresh,]
 for(row.i in 1:nrow(pred.thresh.only)){
   r <- pred.thresh.only[row.i, ]
   viz$first[[paste0(r$model.name, ".thresh")]] <- r$mid.thresh
