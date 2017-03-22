894c081c27480262231be26168f835a3a9b4dcd5
Toby Dylan Hocking <tdhock5@gmail.com>
2017-03-22 13:35:55 -0400

    new survival works

--- neuroblastomaProcessed.trafotree.R
+++ neuroblastomaProcessed.trafotree.R
@@ -50,13 +50,13 @@ works_with_R <- function(Rvers,...){
   }
 }
 works_with_R(
-  "3.3.2",
+  "3.3.3",
   partykit="2.0.2",#R CMD INSTALL partykit/pkg/devel/partykit/
   libcoin="0.9.1",
   mlt="0.1.3",
   trtf="0.1.1",#R CMD INSTALL ctm/pkg/trtf/
-  "tdhock/penaltyLearning@82b16a3c204713818cc23517af85b3a92516af87")
-library(survival)
+  "tdhock/penaltyLearning@82b16a3c204713818cc23517af85b3a92516af87",
+  survival="2.41.2")
 
 data(neuroblastomaProcessed, package="penaltyLearning")
 finite.targets <- with(neuroblastomaProcessed, {
@@ -89,16 +89,33 @@ tree.fit <- trafotree(
   mltargs=list(theta=coef(mlt.fit)))
 predict(tree.fit)
 
-## Train on all n=3418 observations and p=2 features => Error in
-## normalizetime(Y) : dims [product 10254] do not match the length of
-## object [9681]
+## Train on all n=3418 observations and p=2 features => learns
+## non-trivial model.
 train.feature.mat <- neuroblastomaProcessed$feature.mat[, c("log.n", "log.mad")]
 train.df <- data.frame(log.penalty=train.Surv, train.feature.mat)
 mlt.fit <- mlt(m, data=train.df)
+tree.fit <- trafotree(
+  m, formula = log.penalty ~ ., data=train.df,
+  mltargs=list(theta=coef(mlt.fit)))
+node_party(tree.fit) # 15-node tree.
+tree.fit$coef # 8 leaf nodes.
+
+## I'm not sure if this is the right way to get predicted output
+## values:
+pred.vec <- predict(tree.fit)
+pred.log.penalty <- tree.fit$coef[names(pred.vec), "(Intercept)"]
+is.lo <- pred.log.penalty < neuroblastomaProcessed$target.mat[, 1]
+is.hi <- neuroblastomaProcessed$target.mat[, 2] < pred.log.penalty
+is.error <- is.lo | is.hi
+mean(is.error)
 
 ## Ultimately, I would like to train on all n=3418 observations and
-## all p=117 features. Error in normalizetime(Y) : dims [product
-## 10254] do not match the length of object [9681]
+## all p=117 features.
 train.df <- data.frame(
   log.penalty=train.Surv, neuroblastomaProcessed$feature.mat)
 mlt.fit <- mlt(m, data=train.df)
+tree.fit <- trafotree(
+  m, formula = log.penalty ~ ., data=train.df,
+  mltargs=list(theta=coef(mlt.fit)))
+node_party(tree.fit) # 19-node tree.
+tree.fit$coef 
